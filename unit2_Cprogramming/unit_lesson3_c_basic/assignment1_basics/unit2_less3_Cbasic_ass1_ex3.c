97b9bc309de8304133ad8df610924d7a3787a6dd
Mahmoud Ayman <160341295+Ma7moud-2ymann@users.noreply.github.com>
2024-02-24 10:59:35 +0200

    Add files via upload

---
+++ unit2_Cprogramming/unit_lesson3_c_basic/assignment1_basics/unit2_less3_Cbasic_ass1_ex3.c
@@ -0,0 +1,23 @@
+
+/*
+ ======================================================================================================================
+ Name        : unit2_less3_Cbasic_ass1_ex3
+ Author      : Mahmoud Ayman
+ Created on	 : 24-2-2024
+Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX3 C Program to Add Two Integers
+ ================== ====================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x,y,sum ;
+    printf("enter two integers : ");
+    scanf("%d %d",&x,&y);
+    sum=x+y;
+    printf("sum:%d",sum);
+
+    return 0;
+}
