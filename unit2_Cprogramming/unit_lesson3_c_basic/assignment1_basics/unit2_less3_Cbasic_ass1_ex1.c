304499adc2665c84f27748b53142889ce96c6b70
Mahmoud Ayman <160341295+Ma7moud-2ymann@users.noreply.github.com>
2024-02-24 10:50:25 +0200

    Add files via upload

---
+++ unit2_Cprogramming/unit_lesson3_c_basic/assignment1_basics/unit2_less3_Cbasic_ass1_ex1.c
@@ -0,0 +1,19 @@
+
+/*
+ ======================================================================================================================
+ Name        : unit2_less3_Cbasic_ass1_ex1
+ Author      : Mahmoud Ayman Ali
+ Created on	 : 24-2-2024
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX1 C Program To Print Sentence
+ ================== ====================================================================================================
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("C Programmming \n");
+    return 0;
+}
