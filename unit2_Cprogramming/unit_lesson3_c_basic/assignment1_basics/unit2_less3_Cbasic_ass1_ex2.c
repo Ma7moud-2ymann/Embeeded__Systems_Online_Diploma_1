72879a1e59ab6c735c0fb7dcc5d5d3690c641c78
Mahmoud Ayman <160341295+Ma7moud-2ymann@users.noreply.github.com>
2024-02-24 10:56:22 +0200

    Add files via upload

---
+++ unit2_Cprogramming/unit_lesson3_c_basic/assignment1_basics/unit2_less3_Cbasic_ass1_ex2.c
@@ -0,0 +1,23 @@
+
+
+/*
+ ======================================================================================================================
+ Name        : unit2_less3_Cbasic_ass1_ex2
+ Author      : Mahmoud Ayman
+ Created on	 : Feb 22 2024
+ Description : Unit 2 C-Progtamming >> Assignment 1 C-Basic >> EX2 C Program To Print a Integer Entered by a User
+ ================== ====================================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    printf("Enter a integer:");
+    scanf("%d",&x);
+    printf("You entered:%d",x);
+
+    return 0;
+}
